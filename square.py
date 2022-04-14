9e5b7ba5f54852f9a0cc5dc62a57d4082d77748b
Ojunde Stephen Mayowa <stephenmayowa112@gmail.com>
2022-04-14 16:40:40 +0100

    commit

---
+++ square.py
@@ -0,0 +1,12 @@
+square_list = {}
+for x in range(10):
+    square_list[x] = x**2
+print(square_list)     
+
+square_play = {x : x**2 for x in range (10)}
+print(square_play)
+
+squares = []
+for x in range(10):
+    squares.append(x**2)
+print(squares)     
---
+++ square_list.py
@@ -0,0 +1,5 @@
+square_list = {}
+for x in range(10):
+    square_list[x] = x**2
+    
+print(square_list)    
